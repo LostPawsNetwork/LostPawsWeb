59fa0837f2b069ef04cae5e4b2a6138dfc684a1a
ximena-ortiz <xo2021071080@virtual.upt.pe>
2024-05-01 19:12:37 -0500

    Landing Page

---
+++ src/config/neon.php
@@ -0,0 +1,19 @@
+<?php
+function getPDOConnection()
+{
+    $host = "";
+    $db = "";
+    $user = "";
+    $pass = "";
+    $sslmode = "require";
+
+    $dsn = "pgsql:host=$host;dbname=$db;sslmode=$sslmode";
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass);
+        return $pdo;
+    } catch (\PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        return null;
+    }
+}
