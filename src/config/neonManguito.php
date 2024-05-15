cab0adfb7944b68958109fc1ad66a2521462b48c
mangoesafterplay <ja2021069823@virtual.upt.pe>
2024-05-15 18:50:57 -0500

    Avance noma

--- src/config/neonManguito.php
+++ src/config/neonManguito.php
@@ -1,10 +1,15 @@
 <?php
+    require_once "../../vendor/autoload.php";
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
     function getPDOConnection()
     {
-        $host = "ep-orange-wood-a5jxg7l5.us-east-2.aws.neon.tech";
-        $db = "dblostpaws";
-        $user = "dblostpaws_owner";
-        $pass = "q07TgpWASfwB";
+        $host = $_ENV["DB_HOST"];
+        $db = $_ENV["DB_NAME"];
+        $user = $_ENV["DB_USER"];
+        $pass = $_ENV["DB_PASS"];
         $sslmode = "require";
 
         $dsn = "pgsql:host=$host;dbname=$db;sslmode=$sslmode";
