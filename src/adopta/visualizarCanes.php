28e9bb1a8436119ed78a795a0cf448177f0683a4
robertoGChamUPT <rg2024080298@virtual.upt.pe>
2024-05-30 02:27:02 -0500

    avance modal
    desde el index puedes ir a adoptar, te muestra el listado de canes y si le das en adoptar te abre el modal diciendo que seguimos trabajando

--- src/adopta/visualizarCanes.php
+++ src/adopta/visualizarCanes.php
@@ -1,6 +1,7 @@
 <?php
 // Dearreglo de perros
-//todo hacer consulta a base de datos para traer la info de los perros, ver formato
+//todo hacer consulta a base de datos para traer la info de los perros, ver formato con post nos traemos los datos del filtro
+
 $perros = array(
     array(
         "nombre" => "Georgeth",
@@ -38,99 +39,127 @@ $perros = array(
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Visualizar Canes</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <title>Grid de perros</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="flex">
-        <?php include "../components/header.html"; ?>
-        <div class="flex-1">
-            <!-- Menú Lateral -->
-            <?php include "../components/sidebar.html"; ?>
-
-            <!-- Contenido principal de la página -->
-            <main id="page-content" class="transition-transform duration-300 ease-in-out flex-1 p-4" style="top: 4rem; height: calc(100% - 4rem)">
-                <p>Salto de la linea en la pagina para evitar que se solapen.</p>
-                <br><br>
-                <div class="flex flex-row pt-8  pl-4 pb-6">
-                    <div class="basis-4/5 pr-7">
-                        <div class="grid grid-cols-3 gap-5">
-                            <?php
-                            foreach ($perros as $perro) {
-                                echo "  <div class='text-center shadow-lg shadow-sky-100 outline outline-offset-2 outline-sky-200 rounded'>";
-                                echo "      <div class='h-64'>";
-                                echo "          <img class='rounded-md size-full' src='../assets/imagenes/perros-ejemplo/".$perro['imagen']."' alt='Imagen del canino'>";
-                                echo "      </div>";
-                                echo "      <div class='pb-4 pl-2 h-28'>";
-                                echo "          <h5>".$perro['nombre']."</h5>";
-                                echo "          <p class='text-left'>".$perro['descripcion']."</p>";
-                                echo "      </div>";
-                                echo "      <div class='flex flex-row h-10 bg-gray-200'>";
-                                echo "          <button class='basis-1/2 hover:bg-gray-400'>";
-                                echo "              Donar";
-                                echo "          </button>";
-                                echo "          <button class='basis-1/2 hover:bg-gray-400 '>";
-                                echo "              Adoptar";
-                                echo "          </button>";
-                                echo "      </div>";
-                                echo "  </div>";
-                            }
-                            ?>
+    <div class="flex flex-row pt-8 pr-4 pl-4 pb-6">
+        <div class="basis-5/6 pr-7">
+            <div class="grid grid-cols-3 gap-5">
+                <?php
+                foreach ($perros as $perro) {
+                    echo "  <div class='text-center shadow-lg shadow-sky-100 outline outline-offset-2 outline-sky-200 rounded'>";
+                    echo "      <div class='h-64'>";
+                    echo "          <img class='rounded-md size-full' src='assets/imagenes/perros-ejemplo/".$perro['imagen']."' alt='Imagen del canino'>";
+                    echo "      </div>";
+                    echo "      <div class='pb-4 pl-2 h-28'>";
+                    echo "          <h5>".$perro['nombre']."</h5>";
+                    echo "          <p class='text-left'>".$perro['descripcion']."</p>";
+                    echo "      </div>";
+                    echo "      <div class='flex flex-row h-10 bg-gray-200'>";
+                    if(isset($admin)){
+                        echo "          <button class='w-full hover:bg-gray-400'>";
+                        echo "              Editar can";
+                        echo "          </button>";
+                    }
+                    else{
+                        echo "          <button class='w-full hover:bg-gray-400 ' onclick='verDetalle()'>";
+                        echo "              Adoptar";
+                        echo "          </button>";
+                    }
+                    echo "      </div>";
+                    echo "  </div>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="basis-1/6 pl-3 pr-2 shadow-lg shadow-black-100 outline outline-offset-2 outline-black-200 rounded text-center">
+            <?php
+            //todo cuando se activen los filtros se re carga toda la página para hacer la consulta en base a lo filtrado
+            ?>
+            <fieldset>
+                <div class="mb-2 mt-3 text-2xl text-bold ">
+                    <legend>Filtros</legend>
+                </div>
+                <form action="" method="post" target="_self">
+                    <div>
+                        <label>Tamaño</label>
+                        <div>
+                            <select name="dog-size" id="dog-size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600 dark:placeholder-gray-400">
+                                <option value="">...</option>
+                                <option value="xl">Grande</option>
+                                <option value="x">Mediano</option>
+                                <option value="xs">Pequeño</option>
+                            </select>
+                        </div>
+                        <label>Sexo</label>
+                        <div>
+                            <select name="dog-sex" id="dog-size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600 dark:placeholder-gray-400">
+                                <option value="">...</option>
+                                <option  value="macho">Macho</option>
+                                <option value="hembra">Hembra</option>
+                            </select>
+                        </div>
+                        <label>Edad</label>
+                        <div class="">
+                            <select name="dog-age" id="dog-age" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600 dark:placeholder-gray-400">
+                                <option value="">...</option>
+                                <option value="1">0 a 1 año</option>
+                                <option value="2">1 a 3 años</option>
+                                <option value="3">3 a 6 años</option>
+                                <option value="4">6 o más años</option>
+                            </select>
                         </div>
                     </div>
-                    <div class="basis-1/5 pl-3 pr-2 shadow-lg shadow-black-100 outline outline-offset-2 outline-black-200 rounded text-center">
-                        <?php
-                        //todo cuando se activen los filtros se re carga toda la página para hacer la consulta en base a lo filtrado
-                        ?>
-                        <fieldset>
-                            <div class="mb-2 mt-3 text-2xl text-bold ">
-                                <legend>Filtros</legend>
-                            </div>
-                            <form action="">
-                                <div>
-                                    <label>Tamaño</label>
-                                    <div>
-                                        <select name="dog-size" id="dog-size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600 dark:placeholder-gray-400">
-                                            <option value="">...</option>
-                                            <option value="xl">Grande</option>
-                                            <option value="x">Mediano</option>
-                                            <option value="xs">Pequeño</option>
-                                        </select>
-                                    </div>
-                                    <label>Sexo</label>
-                                    <div>
-                                        <select name="dog-sex" id="dog-size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600 dark:placeholder-gray-400">
-                                            <option value="">...</option>
-                                            <option  value="macho">Macho</option>
-                                            <option value="hembra">Hembra</option>
-                                        </select>
-                                    </div>
-                                    <label>Edad</label>
-                                    <div class="">
-                                        <select name="dog-age" id="dog-age" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600 dark:placeholder-gray-400">
-                                            <option value="">...</option>
-                                            <option value="1">0 a 1 año</option>
-                                            <option value="2">1 a 3 años</option>
-                                            <option value="3">3 a 6 años</option>
-                                            <option value="4">6 o más años</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="outline outline-offset-2 outline-black-100 mt-4 bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">
-                                    <button type="submit">Filtrar</button>
-                                </div>
-                            </form>
-                        </fieldset>
-                    </div>
-                </div>
-            </main>
+                    
+                    <button class="outline outline-offset-2 outline-black-100 mt-4 bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded" type="submit">Filtrar</button>
+                    
+                </form>
+            </fieldset>
         </div>
     </div>
-    <?php include "../components/footer.html"; ?>
-    <script src="scripts/dynamic.js"></script>
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    
+    <button class="mt-5 ml-5 px-4 py-2 bg-blue-600 text-white rounded-md" id="verDetalle">Open Modal</button>
+
+    <!-- En tu archivo PHP -->
+    <div id="myModal" class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center xl:block xl:p-0">
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+            <span class="hidden xl:inline-block xl:align-middle xl:h-screen" aria-hidden="true">&#8203;</span>
+            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all xl:my-8 xl:align-middle xl:max-w-lg xl:w-full">
+                <div class="bg-white px-4 pt-5 pb-4 xl:p-6 xl:pb-4">
+                <div class="xl:flex xl:items-start">
+                    <div class="mt-3 text-center xl:mt-0 xl:ml-4 xl:text-left">
+                    <h3 class="text-xl leading-6 font-large text-gray-900" id="modal-title">Adoptar</h3>
+                    <div class="mt-2">
+                        <p class="text-lg text-gray-500">Seguimos trabajando para traer todos los detalles</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 xl:px-6 xl:flex xl:flex-row-reverse">
+            <button id="saveButton" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-xl px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 xl:ml-3 xl:w-auto xl:text-xl">Adoptar</button>
+            <button id="cancelButton" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-xl px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 xl:mt-0 xl:ml-3 xl:w-auto xl:text-xl">Cerrar</button>
+            </div>
+        </div>
+    </div>
+  </div>
+
 </body>
 </html>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('#verDetalle').click(function(){
+            $('#myModal').removeClass('hidden');
+        });
+        $('#cancelButton').click(function(){
+            $('#myModal').addClass('hidden');
+        })
+    })
     
+    function verDetalle(){
+        console.log('en verdetalle');
+        $('#myModal').removeClass('hidden');
+    }
+</script>
--- src/index.php
+++ src/index.php
@@ -20,100 +20,15 @@
             <main id="page-content" class="transition-transform duration-300 ease-in-out flex-1 p-4" style="top: 4rem; height: calc(100% - 4rem)">
                 <p>Salto de la linea en la pagina para evitar que se solapen.</p>
                 <br><br>
-                <div class="relative w-full h-96 overflow-hidden">
-                    <video autoplay muted loop class="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 z-0">
-                        <source src="assets/videos/landing.mp4" type="video/mp4">
-                        Tu navegador no soporta videos HTML5.
-                    </video>
-                    <div class="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center z-10">
-                        <h1 class="text-white text-3xl font-bold">Lost Paws</h1>
-                    </div>
-                </div>
-
-                <section class="text-center py-10 bg-white">
-                    <div class="container mx-auto">
-                        <h2 class="text-3xl font-bold mb-4">Lost Paws</h2>
-                        <p class="text-gray-700 mb-6">Ofrecemos el mejor servicio veterinario para tus mascotas</p>
-                        <a href="#contacto" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                            Contáctanos
-                        </a>
-                    </div>
-                </section>
-
-                <section id="adopta" class="text-center py-10 bg-white">
-                    <div class="container mx-auto">
-                        <h2 class="text-3xl font-bold mb-4">Adopta una Mascota</h2>
-                        <p class="text-gray-700 mb-6">Explora las opciones para adoptar y dar un hogar a una mascota que lo necesita.</p>
-                    </div>
-                    <a href="adopta/visualizarCanes.php" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                        Adopta
-                    </a>
-                </section>
-
-                <!-- Nosotros -->
-                <section id="nosotros" class="py-10 bg-gray-100">
-                    <div class="container mx-auto text-center">
-                        <h2 class="text-3xl font-bold mb-4">Conoce a nuestro equipo</h2>
-                        <p class="text-gray-700">Un equipo profesional y dedicado a cuidar de tus mascotas.</p>
-                    </div>
-                </section>
-
-                <!-- Sección de Sedes -->
-                <section id="sedes" class="py-10 bg-white">
-                    <div class="container mx-auto">
-                        <h2 class="text-3xl font-bold mb-4">Nuestras Sedes</h2>
-                        <p class="text-gray-700 mb-6">Encuentra nuestra sede más cercana y ven a visitarnos.</p>
-                    </div>
-                    <div id="map" style="height: 400px;"></div>
-                </section>
-                <!-- Sección de Donación -->
-                <section id="donar" class="py-10 bg-gray-100">
-                    <div class="container mx-auto text-center">
-                        <h2 class="text-3xl font-bold mb-4">Cómo Puedes Ayudar</h2>
-                        <p class="text-gray-700">Tu donación hace la diferencia para muchas mascotas.</p><br>
-                        <a href="#donar" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                            Donar
-                        </a>
-                    </div>
-                </section>
-
-                <!-- Sección de Testimonios -->
-                <section id="testimonios" class="py-10 bg-white">
-                    <div class="container mx-auto text-center">
-                        <h2 class="text-3xl font-bold mb-4">Historias de Éxito</h2>
-                        <p class="text-gray-700">Descubre las historias de mascotas y dueños felices gracias a nuestra ayuda.</p>
-                    </div>
-                </section>
-
-                <!-- Sección de Redes Sociales -->
-                <section id="redes" class="py-10 bg-gray-100">
-                    <div class="container mx-auto text-center">
-                        <h2 class="text-3xl font-bold mb-4">Síguenos en Redes</h2>
-                        <p class="text-gray-700">Mantente al día con nuestras últimas noticias y eventos en nuestras redes sociales.</p>
-                    </div>
-                </section>
-
-                <!-- Sección de contacto -->
-                <section id="contacto" class="py-10 bg-gray-100">
-                    <div class="container mx-auto">
-                        <h2 class="text-3xl font-bold text-center mb-6">Contacto</h2>
-                        <form action="#" method="POST" class="max-w-md mx-auto bg-white p-8 shadow rounded">
-                            <div class="mb-4">
-                                <label for="nombre" class="block text-sm font-bold mb-2">Nombre:</label>
-                                <input type="text" id="nombre" name="nombre" class="w-full px-3 py-2 border rounded">
-                            </div>
-                            <div class="mb-4">
-                                <label for="email" class="block text-sm font-bold mb-2">Email:</label>
-                                <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded">
-                            </div>
-                            <div class="mb-4">
-                                <label for="mensaje" class="block text-sm font-bold mb-2">Mensaje:</label>
-                                <textarea id="mensaje" name="mensaje" rows="4" class="w-full px-3 py-2 border rounded"></textarea>
-                            </div>
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-full">Enviar</button>
-                        </form>
-                    </div>
-                </section>
+                <?php //en esta parte recibimos el contenido a mostrar, se debe recibir la ruta con el nombre del archivo sin extensión
+                    if(isset($_GET['content'])){
+                        $path = $_GET['content'];
+                        include "$path.php";
+                    }
+                    else{
+                        include "landing.php";
+                    }
+                ?>
             </main>
         </div>
     </div>
