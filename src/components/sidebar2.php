3d866ea0fe8c16489debcffc8f3b7172b88af785
Camila <camcabrera@upt.pe>
2024-06-23 11:27:25 -0500

    cambios pequeños de nuevo xd

--- src/components/sidebar2.php
+++ src/components/sidebar2.php
@@ -23,18 +23,18 @@ if (isset($_SESSION["correo"])) {
 ?>
 
 <aside id="menu" class="fixed inset-y-0 left-0 transform -translate-x-full bg-blue-800 text-white w-64 overflow-auto transition-transform duration-300 ease-in-out mt-20">
-    <a href="/lostpaws/presentacion/visualizarCanes.php" class="block p-4">Adopta</a>
-    <a href="#nosotros" class="block p-4">Nosotros</a>
-    <a href="#sedes" class="block p-4">Sede</a>
-    <a href="/lostpaws/presentacion/formDonar.php" class="block p-4">Donar</a>
-    <a href="#contacto" class="block p-4">Contactos</a>
-    <a href="#testimonios" class="block p-4">Testimonios</a>
-    <a href="#redes" class="block p-4">Redes</a>
+    <a href="/lostpaws/presentacion/visualizarCanes.php" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Adopta</a>
+    <a href="#nosotros" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Nosotros</a>
+    <a href="#sedes" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Sede</a>
+    <a href="/lostpaws/presentacion/formDonar.php" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Donar</a>
+    <a href="#contacto" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Contactos</a>
+    <a href="#testimonios" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Testimonios</a>
+    <a href="#redes" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Redes</a>
 
     <?php if ($num_adopciones > 0): ?>
-        <a href="/lostpaws/presentacion/misControles.php" class="block p-4">Mis Controles</a>
+        <a href="/lostpaws/presentacion/misControles.php" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Mis Controles</a>
     <?php endif; ?>
 
-    <a href="/lostpaws/presentacion/examenAptitud.php" class="block p-4">Examen Aptitud</a>
-    <a href="/lostpaws/presentacion/editarUsuario.php" class="block p-4">Editar Perfil</a>
+    <a href="/lostpaws/presentacion/examenAptitud.php" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Examen Aptitud</a>
+    <a href="/lostpaws/presentacion/editarUsuario.php" class="block p-5 hover:bg-blue-700 hover:bg-opacity-75 hover:outline-offset-2 shadow-indigo-500/70 hover:shadow-blue-500/70 hover:shadow-lg">Editar Perfil</a>
 </aside>
