4e641d0d19abae24b989b6df9566a3ec12aee1ef
Camila <camcabrera@upt.pe>
2024-06-23 18:49:53 -0500

    cambios dash admin

--- src/presentacion/dashAdmin.php
+++ src/presentacion/dashAdmin.php
@@ -19,65 +19,128 @@ if (
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
   <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    .custom-button {
+        background-color: #1D4ED8; 
+        color: white; 
+        padding: 1rem; 
+        border-radius: 0.5rem; 
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+        width: 95%; 
+    max-width: 450px; 
+    transition: transform 0.2s, background-color 0.2s; 
+        transition: transform 0.2s, background-color 0.2s; 
+        margin: 0 auto; 
+        text-align: center; 
+    }
+
+    .custom-button:hover {
+        background-color: #2563EB; 
+        transform: scale(1.05); 
+    }
+
+    .button-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem; 
+    }
+    
+    #video-background {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      z-index: -1; /* Coloca el video detrás de todo el contenido */
+      filter: blur(10px); /* Desenfoque opcional para el video */
+    }
+
+
+  </style>
 </head>
 
-<body class="bg-gray-100">
+<video id="video-background" autoplay muted loop class="absolute top-0 left-0 w-full h-full object-cover z-0 filter blur-lg">
+        <source src="../assets/videos/ayaha.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <div class="min-h-screen flex items-center justify-center">
+    <div class="bg-blue-300 p-4 sm:p-8 rounded-lg shadow-md w-full max-w-md mx-auto">
+    <div class="flex justify-center items-center">
+               <img src="../assets/images/logoLostPaws.png" alt="Logo" class="h-20" />
+               </div>
+               <br>
 
-    <div class='flex'>
-        <?php include "../components/header3.html"; ?>
+        <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-sm container mx-auto p-4">   
+              
+        <div class="flex flex-col gap-4 flex flex-col gap-4">
+                        <!-- Botón Gestionar Can -->
+                        <a href="gestionarCan.php">
+                            <div class="custom-button">
+                                <div class="font-bold">Gestionar Can</div>
+                            </div>
+                        </a>
 
-        <div class="flex-1">
-            <!-- Contenido principal de la página -->
-            <div id="main-content" class='flex min-h-screen p-4 mt-20 text-center'>
-                <div class="container mx-auto p-4">
-                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
-                        <a href="gestionarCan.php"><div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                            <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                Gestionar Can
+                        <!-- Botón Gestionar Solicitudes -->
+                        <a href="gestionarSolicitudUsuario.php">
+                            <div class="custom-button">
+                                <div class="font-bold">Gestionar Solicitudes</div>
                             </div>
-                        </div></a>
-                        <a href="gestionarSolicitudUsuario.php"><div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                            <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                <button type="submit">Gestionar Solicitudes</button>
+                        </a>
+
+                        <!-- Botón Gestionar Exámenes -->
+                        <a href="gestionarExamenes.php">
+                            <div class="custom-button">
+                                <div class="font-bold">Gestionar Exámenes</div>
                             </div>
-                        </div></a>
-                        <a href="gestionarExamenes.php"><div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                            <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                <button type="submit">Gestionar Exámenes</button>
+                        </a>
+
+                        <!-- Botón Gestionar Controles -->
+                        <a href="gestionarControl.php">
+                            <div class="custom-button">
+                                <div class="font-bold">Gestionar Controles</div>
                             </div>
-                        </div></a>
-                        <a href="gestionarControl.php"><div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                            <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                <button type="submit">Gestionar Controles</button>
-                            </div>
-                        </div></a>
-                        <div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                            <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                <button type="submit">Gestionar Testimonios</button>
-                            </div>
+                        </a>
+
+                        <!-- Botón Gestionar Testimonios -->
+                        <div class="custom-button">
+                            <div class="font-bold">Gestionar Testimonios</div>
                         </div>
-                        <a href="reporteUsuarios.php"><div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                                <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                    <button type="submit">Reporte Usuarios Mal Calificados</button>
-                                </div>
-                        </div></a>
-                        <?php if (
-                            $_SESSION["tipoUsuario"] === "superadmin"
-                        ) { ?>
-                            <a href="gestionarAdministradores.php"><div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                                <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                    <button type="submit">Gestionar Administradores</button>
-                                </div>
-                            </div></a>
-                            <a href="reporteDonaciones.php"><div class="bg-blue-500 hover:bg-blue-600 p-4 rounded-lg shadow-md transform transition duration-500 hover:scale-105">
-                                <div class="bg-white text-black font-bold py-2 px-4 rounded">
-                                    <button type="submit">Reporte Donaciones</button>
-                                </div>
+
+                        <!-- Botón Reporte Usuarios Mal Calificados -->
+                        <a href="reporteUsuarios.php">
+                            <div class="custom-button">
+                                <div class="font-bold">Reporte Usuarios Mal Calificados</div>
                             </div>
+                        </a>
+
+                        <!-- Solo visible para superadmin -->
+                        <?php if ($_SESSION["tipoUsuario"] === "superadmin") { ?>
+                            <!-- Botón Gestionar Administradores -->
+                            <a href="gestionarAdministradores.php">
+                                <div class="custom-button">
+                                    <div class="font-bold">Gestionar Administradores</div>
+                                </div>
+                            </a>
+
+                            <!-- Botón Reporte Donaciones -->
+                            <a href="reporteDonaciones.php">
+                                <div class="custom-button">
+                                    <div class="font-bold">Reporte Donaciones</div>
+                                </div>
+                            </a>
                         <?php } ?>
+                        
                     </div>
                 </div>
-            </div>
+                <br>
+                <a
+            href="/lostpaws/negocio/cerrarSesion.php"
+            class="bg-blue-900 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex justify-center"
+        >
+            Cerrar Sesión
+        </a>
         </div>
     </div>
 
