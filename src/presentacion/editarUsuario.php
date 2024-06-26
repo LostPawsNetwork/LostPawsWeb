ddf7bcd19de8a2b4eacdef892dbd707d01d1b763
ximena-ortiz <xo2021071080@virtual.upt.pe>
2024-06-26 01:51:33 -0500

    avance interfaces usuario

--- src/presentacion/editarUsuario.php
+++ src/presentacion/editarUsuario.php
@@ -10,17 +10,18 @@ if (
     exit();
 }
 
-require_once '../datos/usuario.php';
+require_once "../datos/usuario.php";
 
-$correo = $_SESSION['correo'];
+$correo = $_SESSION["correo"];
 
 $usuario = new Usuario();
 $datosUsuario = $usuario->obtenerUsuario($correo);
 
 if ($datosUsuario !== false && !empty($datosUsuario)) {
-    $nombre = $datosUsuario[0]['nombre'];
-    $apellido = $datosUsuario[0]['apellido'];
+    $nombre = $datosUsuario[0]["nombre"];
+    $apellido = $datosUsuario[0]["apellido"];
 } else {
+    // Manejo de error si no se encuentra el usuario
 }
 ?>
 
@@ -30,38 +31,94 @@ if ($datosUsuario !== false && !empty($datosUsuario)) {
     <meta charset="UTF-8">
     <title>Editar Perfil de Usuario</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: flex-start;
+            min-height: 100vh;
+            position: relative;
+            font-family: 'Inter', sans-serif;
+        }
+
+        .form-container {
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 40px;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            width: 100%;
+            max-width: 800px;
+            margin-top: 100px; /* Ajusta según sea necesario */
+            z-index: 1;
+        }
+
+        .profile-image {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            margin: 0 auto;
+            display: block;
+            margin-bottom: 20px;
+            object-fit: cover;
+        }
+
+        .header {
+            width: 100%;
+            z-index: 2;
+        }
+
+        .sidebar {
+            z-index: 2; /* Asegúrate de que el sidebar esté delante del video */
+        }
+    </style>
 </head>
-<body class="bg-gray-100 flex items-center justify-center min-h-screen">
-    <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h2 class="text-2xl font-bold mb-6 text-center">Editar Perfil de Usuario</h2>
-        <form action="../negocio/procesarEditarUsuario.php" method="post" class="space-y-4">
-            <div>
-                <label for="correo" class="block text-sm font-medium text-gray-700">Correo Electrónico:</label>
-                <input type="email" id="correo" name="correo" class="mt-1 block w-full px-3 py-2 border border-gray-300 
-                rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" 
-                required value="<?php echo htmlspecialchars($correo); ?>">
-            </div>
+<body>
 
-            <div>
-                <label for="nombre" class="block text-sm font-medium text-gray-700">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" class="mt-1 block w-full px-3 py-2 border border-gray-300 
-                rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" 
-                required value="<?php echo htmlspecialchars($nombre); ?>">
-            </div>
+<?php include "../components/header2.html"; ?>
 
-            <div>
-                <label for="apellido" class="block text-sm font-medium text-gray-700">Apellido:</label>
-                <input type="text" id="apellido" name="apellido" class="mt-1 block w-full px-3 py-2 border border-gray-300 
-                rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" 
-                required value="<?php echo htmlspecialchars($apellido); ?>">
-            </div>
+<video id="video-background" autoplay muted loop class="absolute top-0 left-0 w-full h-full object-cover z-0 filter blur-lg">
+    <source src="../assets/videos/editarPerfil.mp4" type="video/mp4">
+</video>
+<div class="sidebar">
+    <?php include "../components/sidebar2.php"; ?>
+</div>
 
-            <div class="flex justify-center">
-                <button type="submit" class="w-full py-2 px-4 bg-indigo-600 text-white font-medium rounded-md shadow-sm 
-                hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Actualizar Perfil</button>
-            </div>
-        </form>
-    </div>
+<div class="form-container mt-28">
+    <img src="../assets/images/perfil.jpg" alt="Profile Picture" class="profile-image">
+    <h2 class="text-3xl font-bold mb-6 text-center">Editar Perfil de Usuario</h2>
+    <form action="../negocio/procesarEditarUsuario.php" method="post" class="space-y-6">
+        <div>
+            <label for="correo" class="block text-sm font-medium text-gray-700">Correo Electrónico:</label>
+            <input type="email" id="correo" name="correo" class="mt-1 block w-full px-3 py-2 border border-gray-300
+            rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required value="<?php echo htmlspecialchars($correo); ?>">
+        </div>
+
+        <div>
+            <label for="nombre" class="block text-sm font-medium text-gray-700">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" class="mt-1 block w-full px-3 py-2 border border-gray-300
+            rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required value="<?php echo htmlspecialchars($nombre); ?>">
+        </div>
+
+        <div>
+            <label for="apellido" class="block text-sm font-medium text-gray-700">Apellido:</label>
+            <input type="text" id="apellido" name="apellido" class="mt-1 block w-full px-3 py-2 border border-gray-300
+            rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            required value="<?php echo htmlspecialchars($apellido); ?>">
+        </div>
+
+        <div class="flex justify-center">
+            <button type="submit" class="bg-blue-600 text-white w-full p-3 rounded-md hover:bg-blue-700">Actualizar Perfil</button>
+        </div>
+    </form>
+</div>
+
+<?php include "../components/footer.html"; ?>
+
+<script src="../scripts/dynamic.js"></script>
 </body>
 </html>
-
--- src/presentacion/restablecerPassword.php
+++ src/presentacion/restablecerPassword.php
@@ -16,7 +16,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bindParam(":correo", $correo);
         $stmt->execute();
 
-        echo "Contraseña actualizada exitosamente. <a href='login.php'>Iniciar sesión</a>";
+        echo "<script>
+                alert('Contraseña actualizada exitosamente.');
+                window.location.href = 'login.php';
+              </script>";
         exit();
     } else {
         $error = "Las contraseñas no coinciden.";
@@ -35,38 +38,48 @@ $correo = isset($_GET["correo"])
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restablecer Contraseña</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-100 h-screen font-sans">
+<body class="bg-black h-screen font-sans relative overflow-hidden">
+    <video id="video-background" autoplay muted loop class="absolute top-0 left-0 w-full h-full object-cover z-0 filter blur-lg">
+        <source src="../assets/videos/login.mp4" type="video/mp4">
+    </video>
 
-    <div class="container mx-auto p-4">
-        <div class="bg-white p-6 rounded-lg shadow-md space-y-4">
-            <h1 class="text-2xl font-bold mb-4">Restablecer Contraseña</h1>
-            <?php if (isset($error)): ?>
-                <p class="text-red-600"><?php echo htmlspecialchars(
-                    $error,
-                    ENT_QUOTES,
-                    "UTF-8"
-                ); ?></p>
-            <?php endif; ?>
-            <form action="restablecerPassword.php" method="post" class="space-y-4">
-                <div>
-                    <label for="nuevaContrasena" class="block text-sm font-medium text-gray-700">Nueva Contraseña</label>
-                    <input type="password" name="nuevaContrasena" id="nuevaContrasena" required class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 focus:border-blue-300">
-                </div>
-                <div>
-                    <label for="repetirContrasena" class="block text-sm font-medium text-gray-700">Repetir Contraseña</label>
-                    <input type="password" name="repetirContrasena" id="repetirContrasena" required class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 focus:border-blue-300">
-                </div>
-                <input type="hidden" name="correo" value="<?php echo $correo; ?>">
-                <div class="flex justify-end">
-                    <button type="submit" class="bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700">Restablecer Contraseña</button>
-                </div>
-            </form>
+    <div class="absolute inset-0 bg-black opacity-50 z-10"></div>
+
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30 w-full px-4">
+        <div class="bg-blue-300 p-4 sm:p-8 rounded-lg shadow-md w-full max-w-md mx-auto">
+            <div class="flex justify-center items-center">
+                <img src="../assets/images/logoLostPaws.png" alt="Logo" class="h-20" />
+            </div>
+            <br>
+            <div class="bg-blue-100 p-6 rounded-lg">
+                <h2 class="text-xl sm:text-2xl font-semibold mb-4 text-gray-800 text-center">Restablecer Contraseña</h2>
+                <?php if (isset($error)): ?>
+                    <p class="text-red-600"><?php echo htmlspecialchars(
+                        $error,
+                        ENT_QUOTES,
+                        "UTF-8"
+                    ); ?></p>
+                <?php endif; ?>
+                <form action="restablecerPassword.php" method="post" class="space-y-4">
+                    <div>
+                        <label for="nuevaContrasena" class="block text-sm font-medium text-gray-700">Nueva Contraseña</label>
+                        <input type="password" name="nuevaContrasena" id="nuevaContrasena" required class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 focus:border-blue-300">
+                    </div>
+                    <div>
+                        <label for="repetirContrasena" class="block text-sm font-medium text-gray-700">Repetir Contraseña</label>
+                        <input type="password" name="repetirContrasena" id="repetirContrasena" required class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200 focus:border-blue-300">
+                    </div>
+                    <input type="hidden" name="correo" value="<?php echo $correo; ?>">
+                    <div class="flex justify-center">
+                        <button type="submit" class="bg-blue-400 text-white w-full sm:w-60 h-12 p-2 rounded-md hover:bg-blue-500">Restablecer Contraseña</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-
 </body>
 
 </html>
